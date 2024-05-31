8dacea51a396173ed3d617efb73051a672318d37
Slime <mascarinas022@gmail.com>
2024-05-31 09:40:49 +0800

    set up drizzle and connection to the database

---
+++ src/drizzle/schema.ts
@@ -0,0 +1,5 @@
+import {pgTable,varchar, uuid } from "drizzle-orm/pg-core"
+export const UserTable = pgTable("user", {
+   id: uuid("id").primaryKey().defaultRandom(),
+   name: varchar("name", {length: 255}).notNull()
+})
